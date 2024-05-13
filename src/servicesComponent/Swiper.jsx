1d17d3c471ab3cee7ce6883e28afedba5f7e6763
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-05-13 19:12:25 +0530

    mamaearth images on slider

--- src/servicesComponent/Swiper.jsx
+++ src/servicesComponent/Swiper.jsx
@@ -17,21 +17,21 @@ import asset9 from "../assets/asset9.jpeg";
 import asset10 from "../assets/asset10.jpeg";
 import asset11 from "../assets/asset11.jpeg";
 import asset12 from "../assets/asset12.gif";
-import adna1 from "../assets/adna1.jpg";
-import adna2 from "../assets/adna2.jpg";
-import adna3 from "../assets/adna3.jpg";
-import adna4 from "../assets/adna4.jpg";
+// import adna1 from "../assets/adna1.jpg";
+// import adna2 from "../assets/adna2.jpg";
+// import adna3 from "../assets/adna3.jpg";
+// import adna4 from "../assets/adna4.jpg";
 
 const SwiperComponent = () => {
   const [loading, setLoading] = useState(true); // Initially set loading to true
 
   const swiperImages = [
-    // asset8, asset9, asset10, asset11, asset12
+    asset8, asset9, asset10, asset11, asset12
 
-    adna1,
-    adna2,
-    adna3,
-    adna4,
+    // adna1,
+    // adna2,
+    // adna3,
+    // adna4,
   ];
 
   // Simulating image loading with useEffect
